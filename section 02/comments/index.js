4b635fc3e3b9789bd665fe7e619cfb7629567f5b
Hamzah9797 <hamzahhameed9797@gmail.com>
2023-07-12 17:19:11 +0700

    moderation service updated

--- section 02/comments/index.js
+++ section 02/comments/index.js
@@ -31,15 +31,39 @@ app.post("/posts/:id/comments", (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-// Receive events from event bus
-app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
+// Receive event from event bus that comment has been moderated
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    // Emit an event to the event bus that comment has been updated
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
