c9d1316dd662586c5a07b49dc54e451ac86e4e3b
Hamzah9797 <hamzahhameed9797@gmail.com>
2023-07-12 17:54:21 +0700

    event sync

--- section 02/event-bus/index.js
+++ section 02/event-bus/index.js
@@ -5,9 +5,13 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 // This is the event bus
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
@@ -26,6 +30,10 @@ app.post("/events", (req, res) => {
   });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
